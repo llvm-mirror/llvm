c9b938065aed1d6a613daa296d0cdb9257ee6ffa
Chris Lattner <sabre@nondot.org>
2004-10-11 20:53:28 +0000

    Make this build
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@16919 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Analysis/DataStructure/EquivClassGraphs.cpp
+++ lib/Analysis/DataStructure/EquivClassGraphs.cpp
@@ -21,11 +21,11 @@
 #include "llvm/Analysis/DataStructure/DSGraph.h"
 #include "llvm/Analysis/DataStructure/DataStructure.h"
 #include "llvm/Support/CallSite.h"
-#include "Support/Debug.h"
-#include "Support/SCCIterator.h"
-#include "Support/Statistic.h"
-#include "Support/EquivalenceClasses.h"
-#include "Support/STLExtras.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/ADT/SCCIterator.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/EquivalenceClasses.h"
+#include "llvm/ADT/STLExtras.h"
 using namespace llvm;
 
 namespace llvm {
