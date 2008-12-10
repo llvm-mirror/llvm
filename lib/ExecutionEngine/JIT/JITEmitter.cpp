llvm-mirror/llvm

commit f8ba919d76e46af7ab557d3c604d84ea3083c8e1

Always emit a function pointer as a pointer to the function stub (if there is one). This makes it possible to compare function pointer values in lazy compilation mode. This fixes PR3043.
llvm-svn: 59265

lib/ExecutionEngine/JIT/JITEmitter.cpp
@@ -115,6 +115,10 @@ namespace {
       TheJITResolver = 0;
     }
 
+    /// getFunctionStubIfAvailable - This returns a pointer to a function stub
+    /// if it has already been created.
+    void *getFunctionStubIfAvailable(Function *F);
+
     /// getFunctionStub - This returns a pointer to a function stub, creating
     /// one on demand as needed.
     void *getFunctionStub(Function *F);
@@ -150,6 +154,16 @@ namespace {
 }
 
 JITResolver *JITResolver::TheJITResolver = 0;
+
+/// getFunctionStubIfAvailable - This returns a pointer to a function stub
+/// if it has already been created.
+void *JITResolver::getFunctionStubIfAvailable(Function *F) {
+  MutexGuard locked(TheJIT->lock);
+
+  // If we already have a stub for this function, recycle it.
+  void *&Stub = state.getFunctionToStubMap(locked)[F];
+  return Stub;
+}
 
 /// getFunctionStub - This returns a pointer to a function stub, creating
 /// one on demand as needed.
@@ -596,7 +610,12 @@ void *JITEmitter::getPointerToGlobal(GlobalValue *V, void *Reference,
 
   // If we have already compiled the function, return a pointer to its body.
   Function *F = cast<Function>(V);
-  void *ResultPtr = TheJIT->getPointerToGlobalIfAvailable(F);
+  void *ResultPtr;
+  if (!DoesntNeedStub)
+    // Return the function stub if it's already created.
+    ResultPtr = Resolver.getFunctionStubIfAvailable(F);
+  else
+    ResultPtr = TheJIT->getPointerToGlobalIfAvailable(F);
   if (ResultPtr) return ResultPtr;
 
   if (F->isDeclaration() && !F->hasNotBeenReadFromBitcode()) {

commit 1264f4bc9c4d0846d622f6078391da9351ea89a3

Fix a bug introduced by r59265. If lazy compilation is disabled, return actual function ptr instead of ptr to stub if function is already compiled.
llvm-svn: 60822

lib/ExecutionEngine/JIT/JITEmitter.cpp
@@ -611,7 +611,7 @@ void *JITEmitter::getPointerToGlobal(GlobalValue *V, void *Reference,
   // If we have already compiled the function, return a pointer to its body.
   Function *F = cast<Function>(V);
   void *ResultPtr;
-  if (!DoesntNeedStub)
+  if (!DoesntNeedStub && !TheJIT->isLazyCompilationDisabled())
     // Return the function stub if it's already created.
     ResultPtr = Resolver.getFunctionStubIfAvailable(F);
   else