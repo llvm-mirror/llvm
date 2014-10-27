llvm-mirror/llvm

commit 87efb91a502cdf39ce6d3f3a5c8ac5d6630fe0af

Stackmap shadows should consider call returns a branch target.
To avoid emitting too many nops, a stackmap shadow can include emitted instructions in the shadow, but these must not include branch targets.

A return from a call should count as a branch target as patching over the instructions after the call would lead to incorrect behaviour for threads currently making that call, when they return.

llvm-svn: 220710

lib/Target/X86/X86MCInstLower.cpp
@@ -1210,4 +1210,10 @@ void X86AsmPrinter::EmitInstruction(const MachineInstr *MI) {
   MCInst TmpInst;
   MCInstLowering.Lower(MI, TmpInst);
   EmitAndCountInstruction(TmpInst);
+
+  // Stackmap shadows cannot include branch targets, so we can count the bytes
+  // in a call towards the shadow, but must flush the shadow immediately after
+  // to account for the return from the call.
+  if (MI->isCall())
+    SMShadowTracker.emitShadowPadding(OutStreamer, getSubtargetInfo());
 }

test/CodeGen/X86/stackmap-shadow-optimization.ll
@@ -1,16 +1,18 @@
 ; RUN: llc < %s -mtriple=x86_64-apple-darwin -mcpu=corei7 | FileCheck %s
 
-; Check that the X86 stackmap shadow optimization is only outputting a 1-byte
-; nop here. 8-bytes are requested, but 7 are covered by the code for the call to
-; bar, the frame teardown and the return.
+; Check that the X86 stackmap shadow optimization is only outputting a 3-byte
+; nop here. 8-bytes are requested, but 5 are covered by the code for the call to
+; bar.  However, the frame teardown and the return do not count towards the
+; stackmap shadow as the call return counts as a branch target so must flush
+; the shadow.
 define void @shadow_optimization_test() {
 entry:
 ; CHECK-LABEL:  shadow_optimization_test:
 ; CHECK:        callq   _bar
 ; CHECK-NOT:    nop
 ; CHECK:        callq   _bar
-; CHECK:        retq
 ; CHECK:        nop
+; CHECK:        retq
   call void @bar()
   tail call void (i64, i32, ...)* @llvm.experimental.stackmap(i64 0, i32 8)
   call void @bar()

commit 01b213297228c450181f5038f39168bcf153e196

Fix a stackmap bug introduced in r220710.
For a call to not return in to the stackmap shadow, the shadow must end with the call.

To do this, we must insert any required nops *before* the call, and not after it.

llvm-svn: 220728

lib/Target/X86/X86MCInstLower.cpp
@@ -1209,11 +1209,21 @@ void X86AsmPrinter::EmitInstruction(const MachineInstr *MI) {
 
   MCInst TmpInst;
   MCInstLowering.Lower(MI, TmpInst);
-  EmitAndCountInstruction(TmpInst);
 
   // Stackmap shadows cannot include branch targets, so we can count the bytes
-  // in a call towards the shadow, but must flush the shadow immediately after
-  // to account for the return from the call.
-  if (MI->isCall())
+  // in a call towards the shadow, but must ensure that the no thread returns
+  // in to the stackmap shadow.  The only way to achieve this is if the call
+  // is at the end of the shadow.
+  if (MI->isCall()) {
+    // Count then size of the call towards the shadow
+    SMShadowTracker.count(TmpInst, getSubtargetInfo());
+    // Then flush the shadow so that we fill with nops before the call, not
+    // after it.
     SMShadowTracker.emitShadowPadding(OutStreamer, getSubtargetInfo());
+    // Then emit the call
+    OutStreamer.EmitInstruction(TmpInst, getSubtargetInfo());
+    return;
+  }
+
+  EmitAndCountInstruction(TmpInst);
 }

test/CodeGen/X86/stackmap-shadow-optimization.ll
@@ -5,16 +5,21 @@
 ; bar.  However, the frame teardown and the return do not count towards the
 ; stackmap shadow as the call return counts as a branch target so must flush
 ; the shadow.
+; Note that in order for a thread to not return in to the patched space
+; the call must be at the end of the shadow, so the required nop must be
+; before the call, not after.
 define void @shadow_optimization_test() {
 entry:
 ; CHECK-LABEL:  shadow_optimization_test:
 ; CHECK:        callq   _bar
+; CHECK:        nop
+; CHECK:        callq   _bar
 ; CHECK-NOT:    nop
 ; CHECK:        callq   _bar
-; CHECK:        nop
-; CHECK:        retq
+; CHECK-NOT:    nop
   call void @bar()
   tail call void (i64, i32, ...)* @llvm.experimental.stackmap(i64 0, i32 8)
+  call void @bar()
   call void @bar()
   ret void
 }