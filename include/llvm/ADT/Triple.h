ffe6ad31af3cc98b4a09949ae690005f6430161d
bryant <bryant@users.noreply.github.com>
2018-03-29 17:09:16 -0700

    add cb2 target triple.

--- include/llvm/ADT/Triple.h
+++ include/llvm/ADT/Triple.h
@@ -54,6 +54,7 @@ public:
     avr,            // AVR: Atmel AVR microcontroller
     bpfel,          // eBPF or extended BPF or 64-bit BPF (little endian)
     bpfeb,          // eBPF or extended BPF or 64-bit BPF (big endian)
+    cb2,            // C Backend 2
     hexagon,        // Hexagon: hexagon
     mips,           // MIPS: mips, mipsallegrex
     mipsel,         // MIPSEL: mipsel, mipsallegrexel
