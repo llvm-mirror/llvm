c925561228cd5cc392256179ba3b3b46ea2a6a13
Jian Weng <jian.weng@ucla.edu>
2018-12-19 18:52:58 -0800

    commit to stash

--- ss-example/abcd.cc
+++
@@ -1,8 +0,0 @@
-void vec_add(int n, int *a, int *b, int *c, int *d) {
-  #pragma ss config
-  #pragma ss stream
-  #pragma ss dfg dedicated unroll(4)
-  for (int i = 0; i < n; i++) {
-    d[i] += a[i] * b[i] + c[i];
-  }
-}
--- ss-example/vec_add.cc
+++
@@ -1,8 +0,0 @@
-void vec_add(int *a, int *b, int *c) {
-  #pragma ss config
-  #pragma ss stream
-  #pragma ss dfg dedicated unroll(4)
-  for (int i = 0; i < 128; i++) {
-    c[i] = a[i] + b[i];
-  }
-}
