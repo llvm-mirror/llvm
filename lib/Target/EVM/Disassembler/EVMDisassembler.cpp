2f465bb57e8b8980911f59108cfd1a3ba2336d90
Alan Li <alan.li@me.com>
2019-07-27 22:15:16 +0800

    Implement disassembler details.

--- lib/Target/EVM/Disassembler/EVMDisassembler.cpp
+++ lib/Target/EVM/Disassembler/EVMDisassembler.cpp
@@ -60,13 +60,47 @@ extern "C" void LLVMInitializeEVMDisassembler() {
 
 #include "EVMGenDisassemblerTables.inc"
 
+static unsigned get_push_opcode(unsigned s) {
+  switch (s) {
+    default:
+      llvm_unreachable("incorrect size or unimplemented");
+    case 1: return EVM::PUSH1;
+    case 2: return EVM::PUSH2;
+    case 3: return EVM::PUSH3;
+    case 4: return EVM::PUSH4;
+    case 5: return EVM::PUSH5;
+    case 6: return EVM::PUSH6;
+    case 7: return EVM::PUSH7;
+    case 8: return EVM::PUSH8;
+  }
+}
+
 DecodeStatus EVMDisassembler::getInstruction(MCInst &Instr, uint64_t &Size,
                                              ArrayRef<uint8_t> Bytes,
                                              uint64_t Address,
                                              raw_ostream &VStream,
                                              raw_ostream &CStream) const {
-  DecodeStatus Result;
+  DecodeStatus Result = DecodeStatus::Fail;
 
+  // handle PUSH
+  unsigned opcode = Bytes[0];
+  if (opcode >= 0x60 && opcode <= 0x7f) {
+    unsigned length = opcode - 0x60 + 1;
+    unsigned opcode = get_push_opcode(length);
+     
+    for (unsigned i = 0; i < length; ++i) {
+
+    }
+
+    return DecodeStatus::Success;
+  }
+
+  //Result = decodeInstruction(DecoderTable8, MI, Insn, Address, this, STI);
+  LLVM_DEBUG({
+    if (Result != DecodeStatus::Success) {
+      dbgs() << "Unsuccessfully decoding at: " << Address << "\n";
+    }
+  });
   return Result;
 }
 
