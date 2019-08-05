1d7f39b8f656eca17750f97f59e5be172f6273a6
Alan Li <alan.li@me.com>
2019-08-05 18:25:57 +0800

    fix disassembler immediate value parsing bug.

--- lib/Target/EVM/Disassembler/EVMDisassembler.cpp
+++ lib/Target/EVM/Disassembler/EVMDisassembler.cpp
@@ -69,24 +69,27 @@ DecodeStatus EVMDisassembler::getInstruction(MCInst &Instr, uint64_t &Size,
 
   // handle PUSH
   unsigned opcode = Bytes[0];
+
   if (opcode >= 0x60 && opcode <= 0x7f) {
     unsigned length = opcode - 0x60 + 1;
     unsigned opcode = EVMSubtarget::get_push_opcode(length);
+    Instr.clear();
     Instr.setOpcode(opcode);
     Size = 1 + length;
 
-    auto &Op = Instr.getOperand(1);
     if (length > 8) {
-      // we will have to use a larger container than 64bit.
-      llvm_unreachable("PUSH operand too large, unimplemented");
+      APInt cimm;
+      for (unsigned i = 0; i < length; ++i) {
+        cimm = cimm << 8 + Bytes[1 + i];
+      }
+      llvm_unreachable("unimplemented");
     } else {
       uint64_t imm = 0;
       for (unsigned i = 0; i < length; ++i) {
         imm = (imm << i) + Bytes[1 + i];
       }
-      Op.setImm(imm);
+      Instr.addOperand(MCOperand::createImm(imm));
     }
-
     return DecodeStatus::Success;
   }
 
