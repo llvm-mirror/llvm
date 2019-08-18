e6b23e37d23d611dbb8afe700bec3826000a5328
Alan Li <alan.li@me.com>
2019-08-18 15:55:50 -0400

    Adding a utilities file.

---
+++ lib/Target/EVM/EVMUtils.h
@@ -0,0 +1,33 @@
+//===-- EVMUtilities - EVM Utility Functions ---*- C++ -*-====//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of the EVM-specific
+/// utility functions.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_EVM_EVMUTILITIES_H
+#define LLVM_LIB_TARGET_EVM_EVMUTILITIES_H
+
+#include "llvm/CodeGen/MachineBasicBlock.h"
+
+namespace llvm {
+
+namespace EVM {
+
+
+
+} // end namespace WebAssembly
+
+} // end namespace llvm
+
+#endif
+
+
+
